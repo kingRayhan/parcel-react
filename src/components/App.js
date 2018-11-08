d09ff940558cbc1a4e8ec6ea9ac02adb06739448
kingrayhan <rayhan095@gmail.com>
2018-11-08 12:58:34 +0600

    init

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+export default class App extends React.Component {
+    state = {
+        name: 'Orchie',
+    }
+    render() {
+        return <h1>React {this.rayhan}</h1>
+    }
+}
